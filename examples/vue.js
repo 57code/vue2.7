a56b7f01b648c000a5fbd9f0422e0d24f810381a
yt0379 <yt0379@qq.com>
2022-06-26 13:38:55 +0800

    实战2miniv-vue初始化

--- examples/vue.js
+++ examples/vue.js
@@ -1,4 +1,35 @@
 // mini-vue
-function Vue() {
-  console.log('Vue构造了');
+function Vue(options) {
+  this._init(options)
+}
+
+Vue.prototype._init = function(options) {
+  // options
+  this.$options = options
+  // member init
+  this.$parent = options.parent
+  //...
+  // beforeCreate
+  callHook(this, 'beforeCreate')
+  // state init
+  // ...
+  // created
+  callHook(this, 'created')
+}
+
+function callHook(vm, hook) {
+  const hooks = vm.$options[hook]
+  if (hooks) {
+    hooks.call(vm)
+  }
+}
+
+Vue.prototype.$mount = function(el) {
+  // parent
+  const parent = document.querySelector(el)
+  // data
+  const data = this.$options.data()
+  // append
+  const node = this.$options.render.call(data)
+  parent.appendChild(node)
 }
