12bd42609b1828c0327aaff3f8400b913d85caef
yt0379 <yt0379@qq.com>
2022-06-26 13:14:43 +0800

    手写实战2：初始化

--- examples/vue.js
+++ examples/vue.js
@@ -1,4 +1,33 @@
 // mini-vue
-function Vue() {
-  console.log('Vue构造了');
+function Vue(options) {
+  this._init(options)
+}
+
+Vue.prototype._init = function (options) {
+  this.$options = options
+  // _init() -> member init -> beforeCreate -> state init -> created
+  this.$parent = options.parent
+  callHook(this, 'beforeCreate')
+  // state init
+  // ...
+  callHook(this, 'created')
+}
+
+function callHook(vm, hook) {
+  const hooks = vm.$options[hook]
+  if (hooks) {
+    hooks.call(vm)
+  }
+}
+
+Vue.prototype.$mount = function(el) {
+  // parent
+  const parent = document.querySelector(el)
+
+  // data
+  const data = this.$options.data()
+  
+  // append
+  const node = this.$options.render.call(data)
+  parent.appendChild(node)
 }
